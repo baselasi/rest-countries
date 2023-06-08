71c6b75368feee1e0a131aeb2b520207ec7d4ad3
bassel-new <128236840+bassel-new@users.noreply.github.com>
2023-06-08 23:22:16 +0200

    Update header.js
    adding dark mode btn

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,20 @@
 import React from "react";
 import "./header.css"
 
-export default function Navbar(){
+export default function Navbar(props,{changeState}){
+    const [darckMode,setDarckmode]=React.useState(false)
+    function changeMode(){
+        setDarckmode((prev)=>!prev)
+        props.changeState()
+    }
+    console.log(darckMode)
     return(
-        <div className="header"><span>where in the world?</span><span>darck mode</span></div>
+        <div className={darckMode ? "header darck" : "header"}>
+            <span>where in the world?</span>
+            <div className="header-right">
+                <div className="dark-mode" onClick={changeMode}></div>
+                <span>darck mode</span>
+            </div>
+        </div>
     )
 }
