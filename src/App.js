89fa4de8e35e6ecf403fe6a1e58ca36bc3e06f31
bassel-new <128236840+bassel-new@users.noreply.github.com>
2023-06-10 23:20:45 +0200

    Update App.js
    adding routes

--- src/App.js
+++ src/App.js
@@ -3,18 +3,30 @@ import './App.css';
 import Navbar from './components/header';
 import Countries from './components/countriesList';
 import { useState } from 'react';
+import Home from './components/home';
+import Countrie from './components/singleCoutnrie';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   const [state,setState] = useState(false)
+  const [countrie ,setCountrie] = useState()
+  const [countries,setCountries] = useState()
   console.log(state)
   return (
-    <>
-    <Navbar
-      changeState={()=>setState((prev)=>!prev)}
-    />
-    <Countries
-      darckMode={state}
-    />
-    </>
+    <BrowserRouter>
+      <Navbar
+        changeState={()=>setState((prev)=>!prev)}
+      />
+      <Routes>
+        <Route path='/' element={<Countries 
+        darckMode={state}
+        Countrie={(e)=>setCountrie((prev)=>e)}
+        Countries={setCountries}/>}/>
+        <Route path='/countrie' element={<Countrie
+        countrie={countrie}
+        countries={countries}
+        />}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
