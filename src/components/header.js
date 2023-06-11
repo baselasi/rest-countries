9e96e79df18421526ec0f0a8600990307092c387
bassel-new <128236840+bassel-new@users.noreply.github.com>
2023-06-11 22:15:20 +0200

    Update header.js
    adding comment to header

--- src/components/header.js
+++ src/components/header.js
@@ -1,18 +1,17 @@
 import React from "react";
 import "./header.css"
 
-export default function Navbar(props,{changeState}){
-    const [darckMode,setDarckmode]=React.useState(false)
-    function changeMode(){
-        setDarckmode((prev)=>!prev)
-        props.changeState()
+export default function Navbar({changeState}){
+    const [darckMode,setDarckmode]=React.useState(false)                //false for day mode and true for night mode
+    function changeMode(){                                          //toggle between nigth and day mode
+        setDarckmode((prev)=>!prev)                                   //change mode in header
+        changeState()                                                   //call setstate function in App component
     }
-    console.log(darckMode)
     return(
-        <div className={darckMode ? "header darck" : "header"}>
+        <div className={darckMode ? "header darck" : "header"}>   
             <span>where in the world?</span>
             <div className="header-right">
-                <div className="dark-mode" onClick={changeMode}></div>
+                <div className="dark-mode" onClick={changeMode}></div>  {/* onclick change mode */}
                 <span>darck mode</span>
             </div>
         </div>
