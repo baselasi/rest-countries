42c4ffde532c700c3413958fe4ef9f086ceb2a26
bassel-new <128236840+bassel-new@users.noreply.github.com>
2023-06-13 23:44:50 +0200

    Update countriesList.js
    adding comments

--- src/components/countriesList.js
+++ src/components/countriesList.js
@@ -2,48 +2,45 @@ import React, { useEffect } from "react";
 import "./countriesList.css"
 import { useNavigate } from "react-router-dom";
 export default function Countries(props){
-    const [list,setlist] = React.useState([])
-    const [allCountrie,setAllCountrie] = React.useState([])
-    const [countries,setCountries] = React.useState(0)
-    const [region,setRegion]  = React.useState("")
-    const [countrie,setCountrie] = React.useState("")
+    const [list,setlist] = React.useState([])          //state in wich contiene the country that you want to be  chowen
+    const [allCountrie,setAllCountrie] = React.useState([])  //state that contiene all country objects that have been fetched
+    const [countries,setCountries] = React.useState(0)      //number of country that tou want to be chooen
+    const [region,setRegion]  = React.useState("")              //state to use when serching country by region
     useEffect(()=>{
         const fetchData = async ()  => {
             const dataJson = await fetch("https://restcountries.com/v3.1/all")
             const data = await dataJson.json()
             setAllCountrie(data)
-            if(region===""){
-                for(let i=countries;i<countries+9 && i<data.length;i++){
+            if(region===""){                                              
+                for(let i=countries;i<countries+9 && i<data.length;i++){  //go throught fetched data and show 9 at first then add 9 every time you click the botton "more country"
                     setlist((prev)=>[...prev,data[i]])
                 }
             }
                 else{
-                    const countriesList = data.filter((contrie)=>contrie.region == region)
-                    for(let i=countries;i<countries+9 && i<countriesList.length;i++){
+                    const countriesList = data.filter((contrie)=>contrie.region == region)      //in case you choosen regian filter all the country with that regin   
+                    for(let i=countries;i<countries+9 && i<countriesList.length;i++){  //show 9 country at time
                         setlist((prev)=>[...prev,countriesList[i]])
-                        console.log(list)
                     }
                 }
             }
             fetchData()
     },[countries,region])
-    const navigate = useNavigate()
+    const navigate = useNavigate()  
     function routeChange(object){
         navigate('/countrie')
-        props.Countrie(object)
-        props.Countries(allCountrie)
-        setCountrie(object)
+        props.Countrie(object)  //pass the clicked country's object to the App component so you can pass the countrie component
+        props.Countries(allCountrie)  //paa all fetched country to App component so you can pass the countrie component 
     
     }
-    function showMore(){
+    function showMore(){                //on clcik add 9 to countires state, this will activate useEffect and in return it will add 9 more countrys to list
         setCountries((prev)=>prev+9)
     }
-    function changeRegion(e){
-        setRegion(e)
-        setlist([])
+    function changeRegion(e){    
+        setRegion(e)   //on changing reagion use useeffect will be activate 
+        setlist([])    
         setCountries(0)
     }
-    function chooseCountrie(e){
+    function chooseCountrie(e){         //serching for country by name, this function can be move to useEffect by adding another if condition
         for(let i=0;i<allCountrie.length;i++){
             if(allCountrie[i].name.common===e){
                 setlist([allCountrie[i]])
@@ -51,7 +48,7 @@ export default function Countries(props){
         }            
     }
     return(
-        <div className={props.darckMode ? "main darck" : "main"}>
+        <div className={props.darckMode ? "main darck" : "main"}>  
             <div className="input">
                 <input list="countries" className="list" onChange={()=>chooseCountrie(event.target.value)}/>
                     <datalist   id="countries">
